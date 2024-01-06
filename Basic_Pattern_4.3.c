46a4f8b16834f2a4546858d1045e438df7fc29f3
vamsithabonda <155445021+vamsithabonda@users.noreply.github.com>
2024-01-06 12:05:07 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j;     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {              if(j%2==0)              {                  printf("0 ");              }              else              {                  printf("1 ");              }         }         printf(" ");     } }

---
+++ Basic_Pattern_4.3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+             if(j%2==0)
+             {
+                 printf("0 ");
+             }
+             else
+             {
+                 printf("1 ");
+             }
+        }
+        printf("
+");
+    }
+}
