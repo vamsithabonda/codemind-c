ddd1413227bdd966fe4cb113d8efeb5e4db32922
vamsithabonda <155445021+vamsithabonda@users.noreply.github.com>
2024-01-20 16:34:35 +0530

    #include<stdio.h> int main() {     int n,flag=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int se;     scanf("%d",&se);     for(i=0;i<n;i++)     {         if(se==arr[i])         {             flag=1;             break;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Element_search_in_an_Array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,flag=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int se;
+    scanf("%d",&se);
+    for(i=0;i<n;i++)
+    {
+        if(se==arr[i])
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
