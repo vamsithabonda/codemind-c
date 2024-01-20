5ba53bd94463601bb577a1426b924b96050d3756
vamsithabonda <155445021+vamsithabonda@users.noreply.github.com>
2024-01-20 16:46:26 +0530

    #include<stdio.h> int main() {     int x1,y1,x2,y2;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     int a,b;     scanf("%d%d",&a,&b);     a=(x1+y1);     b=(x2+y2);     if(a<b)     {         printf("%d",a);     }     else     {         printf("%d",b);     } }

---
+++ Interior_Design.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x1,y1,x2,y2;
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a=(x1+y1);
+    b=(x2+y2);
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
